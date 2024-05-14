b288a6979296fb1257ff357832d9cb38e4701040
adelajahalima <adelajahalima@gmail.com>
2024-05-14 17:55:49 +0100

    Added the rest of the files

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Please list 10 commits (from the most recent to oldest) of the repository
+“rails” by the user “rails”
+You must use the GitHub API, here is the documentation
+https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line)
+"""
+
+
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
+
+    repo = argv[1]
+    owner = argv[2]
+    i = 0
+
+    URL = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
